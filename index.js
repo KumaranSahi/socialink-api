dd9e47412f469ae5c372a50c2b2224414101f4c5
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-06-22 14:19:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const router = require("./routes");
 const PORT = 8000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 
 const passportJWT = require("./config/passport");
 
