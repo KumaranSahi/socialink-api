4f473cbe706aa32c1230fc32a18e2e60908651a9
Muthu Kumaran <61352185+KumaranSahi@users.noreply.github.com>
2021-06-24 19:31:21 +0530

    fix-testing changes reverted

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://Kumaran:d4vVpjtTjvYUezrq@socialink.qhrra.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  }
-);
+mongoose.connect(process.env["DB_URI"], {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
 const db = mongoose.connection;
 
